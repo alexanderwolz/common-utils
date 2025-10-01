3f9710fdf2f79f2adc34180a4283043c44260b4e
Alexander Wolz <mail@alexanderwolz.de>
2025-10-01 13:58:12 +0200

    switched to JDK 17

---
+++ src/main/kotlin/de/alexanderwolz/commons/util/StreamUtils.kt
@@ -0,0 +1,18 @@
+package de.alexanderwolz.commons.util
+
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.util.stream.Collectors
+import java.util.stream.Stream
+
+class StreamUtilsTest {
+
+    fun convertToString(stream: InputStream): String {
+        return convertToJavaStream(stream).collect(Collectors.joining("\n"))
+    }
+
+    private fun convertToJavaStream(stream: InputStream): Stream<String> {
+        return BufferedReader(InputStreamReader(stream)).lines()
+    }
+}
---
+++ src/test/kotlin/de/alexanderwolz/commons/util/StreamUtilsTest.kt
@@ -0,0 +1,11 @@
+package de.alexanderwolz.commons.util
+
+import org.junit.jupiter.api.Test
+
+class StreamUtilsTest {
+
+    @Test
+    fun test(){
+    }
+
+}
