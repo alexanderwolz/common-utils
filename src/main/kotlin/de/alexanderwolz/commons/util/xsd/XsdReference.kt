aaa8a781caf368f7490ed001b6a8bbf97786672b
Alexander Wolz <mail@alexanderwolz.de>
2025-10-05 14:46:41 +0200

    no message

--- src/main/kotlin/de/alexanderwolz/commons/util/xsd/XsdReference.kt
+++ src/main/kotlin/de/alexanderwolz/commons/util/xsd/XsdReference.kt
@@ -1,7 +1,13 @@
 package de.alexanderwolz.commons.util.xsd
 
+import java.net.URI
+
 data class XsdReference(
-    val type: String,
+    val type: Type,
     val schemaLocation: String,
-    val namespace: String?
-)
+    val namespace: URI?
+) {
+    enum class Type {
+        IMPORT, INCLUDE, REDEFINE, ROOT
+    }
+}
